c915924f5c2be202067db855dd1e7ea039c4708c
DalyChouikh <chouikhdaly215@gmail.com>
2023-04-25 23:25:54 +0100

    Changed API

--- gpt.js
+++ gpt.js
@@ -1,28 +1,42 @@
-const url = 'https://chatgpt-api7.p.rapidapi.com/ask';
+const url = 'https://openai80.p.rapidapi.com/chat/completions';
 let options = {
     method: 'POST',
     headers: {
         'content-type': 'application/json',
-		'X-RapidAPI-Key': 'cfc127c726msh48ad856ce75b474p14e4d9jsnb9fd891c8a7b',
-		'X-RapidAPI-Host': 'chatgpt-api7.p.rapidapi.com'
+        'X-RapidAPI-Key': 'cfc127c726msh48ad856ce75b474p14e4d9jsnb9fd891c8a7b',
+        'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
     },
     body: JSON.stringify({
-		query: 'Hello!'
+		model: 'gpt-3.5-turbo',
+		messages: [
+			{
+				role: 'user',
+				content: 'What is HTTP error code 404!'
+			}
+		]
 	})
 };
 
 let gpt = {
-    fetchAnswer : function(question){
-        options.body = JSON.stringify({query: question});
+    fetchAnswer: function (question) {
+        options.body = JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'user',
+                    content: question
+                }
+            ]
+        });
         fetch(url, options)
-        .then((response) => response.json())
-        .then((data) => this.displayAnswer(data));
+            .then((response) => response.json())
+            .then((data) => this.displayAnswer(data));
     },
-    displayAnswer : function(data){
-        const message = data.response;
+    displayAnswer: function (data) {
+        const message = data.choices[0].message.content;
         document.querySelector(".answer").innerText = message;
         document.querySelector(".answer").classList.remove("loading");
-        if(document.querySelector(".loader")){
+        if (document.querySelector(".loader")) {
             document.querySelector(".loader").remove();
         }
     },
